3d5ef2dc7ebb227e8f015bd10db3b71dedb30100
Lizzie <lizziefkoehler@gmail.com>
2015-02-20 09:53:44 -0800

    will pass second spec using pepperoni as a property

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-var Pizza = {size: 10, type: "cheese", price: 8};
+var Pizza = {size: 10, pizzaType: "cheese", price: 8};
 Pizza.changePrice = function() {
-    this.price;
+  if (this.pizzaType = "pepperoni") {
+  this.price = this.price + 1.5;
+  }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -4,5 +4,12 @@ describe ('Pizza', function() {
   	var pizzaTest = Object.create(Pizza);
   	expect(pizzaTest.price).to.equal(8);
   	});
+
+  	it("will tell what the price is when the size is default but the type is pepperoni", function(){
+  	var pizzaTest = Object.create(Pizza);
+  	pizzaTest.type = "pepperoni";
+  	pizzaTest.changePrice();
+  	expect(pizzaTest.price).to.equal(9.50);	
+  	});
   });
 });
