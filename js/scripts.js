1b0fbbb33bb9de392d176948f87e61ed8ec2564d
Lizzie <lizziefkoehler@gmail.com>
2015-02-20 10:06:39 -0800

    will pass prics specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-var Pizza = {size: 10, pizzaType: "cheese", price: 8};
+var Pizza = {pizzaSize: 10, pizzaType: "cheese", price: 8};
 Pizza.changePrice = function() {
   if (this.pizzaType = "pepperoni") {
   this.price = this.price + 1.5;
+  }
+  if(this.pizzaSize > 10) {
+    this.price = this.price + this.pizzaSize/4;
+  }
+  else if (this.pizzaSize < 10) {
+    this.price = this.price/2;
   }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -7,9 +7,23 @@ describe ('Pizza', function() {
 
   	it("will tell what the price is when the size is default but the type is pepperoni", function(){
   	var pizzaTest = Object.create(Pizza);
-  	pizzaTest.type = "pepperoni";
+  	pizzaTest.pizzaType = "pepperoni";
   	pizzaTest.changePrice();
   	expect(pizzaTest.price).to.equal(9.50);	
+  	});
+
+  	it("will calculate cost based off number of inches over 10", function(){
+  	var pizzaTest = Object.create(Pizza);
+  	pizzaTest.pizzaSize = 15;
+  	pizzaTest.changePrice();
+  	expect(pizzaTest.price).to.equal(13.25);
+  	});
+
+  	it("will calculate cost based off number of inches under 10", function(){
+  	var pizzaTest = Object.create(Pizza);
+  	pizzaTest.pizzaSize = 5;
+  	pizzaTest.changePrice();
+  	expect(pizzaTest.price).to.equal(4.75);
   	});
   });
 });
